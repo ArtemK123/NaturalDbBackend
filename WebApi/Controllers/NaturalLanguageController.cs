406fa756efec92632e7987fbc016e5ce8b1a3c93
ArtemK123 <darkmode1012@gmail.com>
2022-11-04 01:19:59 +0200

    Added signatures of endpoints

---
+++ WebApi/Controllers/NaturalLanguageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("natural-language")]
+public class NaturalLanguageController : ControllerBase
+{
+    [HttpPost("audio-to-text")]
+    public string AudioToText(IFormFile file)
+    {
+        using var reader = new StreamReader(file.OpenReadStream());
+        var body = reader.ReadToEnd();
+        Console.WriteLine(body);
+        return "Hello World!";
+    }
+
+    [HttpPost("text-to-command")]
+    public string TextToCommand(string text)
+    {
+        Console.WriteLine(text);
+        return text;
+    }
+}
