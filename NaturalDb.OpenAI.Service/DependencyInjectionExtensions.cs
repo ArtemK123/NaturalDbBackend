7127cbf26caca9e95412cee874d9d1a897918070
ArtemK123 <darkmode1012@gmail.com>
2022-11-05 21:53:40 +0200

    Added OpenAI projects

---
+++ NaturalDb.OpenAI.Service/DependencyInjectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NaturalDb.OpenAI.Service;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddOpenAIService(this IServiceCollection services)
+    {
+        return services;
+    }
+}
