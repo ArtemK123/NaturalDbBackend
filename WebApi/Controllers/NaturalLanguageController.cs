ed2b10cf39b8a0e073aa52feedf89c562af6da03
ArtemK123 <darkmode1012@gmail.com>
2022-11-05 22:45:51 +0200

    Added audio to text convertation based on Azure Speech service

--- WebApi/Controllers/NaturalLanguageController.cs
+++ WebApi/Controllers/NaturalLanguageController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace WebApi.Controllers;
 
@@ -6,13 +8,43 @@ namespace WebApi.Controllers;
 [Route("natural-language")]
 public class NaturalLanguageController : ControllerBase
 {
-    [HttpPost("audio-to-text")]
-    public string AudioToText(IFormFile file)
+    private readonly IConfiguration configuration;
+    private readonly ILogger<NaturalLanguageController> logger;
+
+    public NaturalLanguageController(IConfiguration configuration, ILogger<NaturalLanguageController> logger)
     {
-        using var reader = new StreamReader(file.OpenReadStream());
-        var body = reader.ReadToEnd();
-        Console.WriteLine(body);
-        return "Hello World!";
+        this.configuration = configuration;
+        this.logger = logger;
+    }
+
+    [HttpPost("audio-to-text")]
+    public async Task<string> AudioToText(IFormFile file)
+    {
+        var requestId = Guid.NewGuid().ToString();
+        
+        logger.LogInformation($"[{requestId}]: Starting to covert the audio file '{file.FileName}' to text");
+        
+        var tempFileName = Path.GetTempPath() + Guid.NewGuid() + ".wav";
+        await using (var fileStream = new FileStream(tempFileName, FileMode.Create))
+        {
+            await file.CopyToAsync(fileStream);
+        }
+        
+        var speechConfigSectionPath = "Azure:CognitiveServices:Speech";
+        var speechApiKey = configuration.GetSection(speechConfigSectionPath).GetValue<string>("ApiKey");
+        var speechRegion = configuration.GetSection(speechConfigSectionPath).GetValue<string>("Region");
+
+        var speechConfig = SpeechConfig.FromSubscription(speechApiKey, speechRegion);
+        speechConfig.SpeechRecognitionLanguage = "en-US";
+        
+        using var audioInputStream = AudioInputStream.CreatePushStream();
+        using var audioConfig = AudioConfig.FromWavFileInput(tempFileName);
+        using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+     
+        var result = await recognizer.RecognizeOnceAsync();
+
+        logger.LogInformation($"[{requestId}]: Converted text: {result.Text}");
+        return result.Text;
     }
 
     [HttpPost("text-to-command")]
